3cc3ec63b6b893c9c1b1eea267a863ad213aed2d
Sunil Kumar <sunil.dhiman90@gmail.com>
2024-01-26 13:13:44 +0530

    use same MaterialTheme

--- app/src/main/java/com/codingambitions/jetpackcomposeloginapp/MainActivity.kt
+++ app/src/main/java/com/codingambitions/jetpackcomposeloginapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,8 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeLoginAppTheme {
-
+            MaterialTheme {
                App()
             }
         }
--- shared/src/iosMain/kotlin/com/codingambitions/loginapp/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/codingambitions/loginapp/shared/MainViewController.kt
@@ -1,7 +1,10 @@
 package com.codingambitions.loginapp.shared
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.window.ComposeUIViewController
 
 fun MainViewController() = ComposeUIViewController {
-    App()
+    MaterialTheme {
+        App()
+    }
 }
